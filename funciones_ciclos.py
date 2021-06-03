5572c29a491b3344b8870f55beedf90b5921e80d
yeniferbarco <>
2021-06-03 02:52:19 +0000

    Initial commit

---
+++ funciones_ciclos.py
@@ -0,0 +1,27 @@
+""" Modulo Ciclos
+    Funciones para practicas con ciclos
+    Oscar Franco-Bedoya
+    Mayo 10-2021 """
+# Definición de Funciones
+#======================================================================
+#          E S P A C I O    D E    T R A B A J O     A L U M N O
+# ====================================================================
+def simulador_caida_libre(altura):
+  #TODO Comentar código
+  #TODO Implementar la función
+  return "No implementada aún"
+
+def generador_generaciones(generacion):
+  #TODO Comentar código
+  #TODO Implementar la función
+  return "No implementada aún"
+
+def constructor_triangulos(pisos):
+  #TODO Comentar código
+  #TODO Implementar la función
+  return "No implementada aún"
+
+def constructor_tableros(longitud):
+  #TODO Comentar código
+  #TODO Implementar la función
+  return "No implementada aún"
