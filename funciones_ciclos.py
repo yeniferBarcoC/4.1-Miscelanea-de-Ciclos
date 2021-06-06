26971cfd0acf88882f78401d1f605acadde38bf1
Yenifer Barco <>
2021-06-06 23:44:15 +0000

    primer ejercicio resuelto. Ciclo 1, semana 5

--- funciones_ciclos.py
+++ funciones_ciclos.py
@@ -1,16 +1,30 @@
 """ Modulo Ciclos
     Funciones para practicas con ciclos
-    Oscar Franco-Bedoya
-    Mayo 10-2021 """
+    Yenifer Barco Castrillón
+    junio 06-2021 """
 # Definición de Funciones
 #======================================================================
 #          E S P A C I O    D E    T R A B A J O     A L U M N O
 # ====================================================================
 def simulador_caida_libre(altura):
-  #TODO Comentar código
-  #TODO Implementar la función
-  return "No implementada aún"
-
+    """ 
+    Parameters
+    ----------
+    altura:float
+        valor de la altura en metros
+    Return:
+    -------
+   
+    """  
+    distancia=0
+    gravedad=9.8
+    tiempo=0
+    if altura>0:
+        while distancia<=altura:
+            distancia=(1/2)*gravedad*(tiempo**2)
+            print("t=",tiempo,"seg","\td=",distancia,"m")
+            tiempo+=1
+        
 def generador_generaciones(generacion):
   #TODO Comentar código
   #TODO Implementar la función
--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+""" Modulo Ciclos
+    Funciones para practicas con ciclos
+    Yenifer Barco Castrillón
+    junio 06-2021 """
+
+#---------------- Zona librerias------------
+import funciones_ciclos as fc
+
+#======================================================================
+#   Algoritmo principal Punto de entrada a la aplicación (Conquistar)
+# =====================================================================
+
+#Llamado de la funcion de caida libre
+altura = float(input("Por favor ingrese la altura:"))
+fc.simulador_caida_libre(altura)
+
+#Llamado de la fncion de generador de generaciones
+generacion= int(input("\nIngrese el numero de la generación:"))
+fc.generador_generaciones(generacion)
+
+#Llamado de la fncion de constructor de triangilos
+
+#Llamado de la fncion de constructor de tableros
 
